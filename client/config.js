a09a0ec25125eb9025a9b2fd8c49af6779a8156f
sumn2u <sumn2u@gmail.com>
2024-06-11 13:28:22 -0500

    added test case for download button

--- client/config.js
+++ client/config.js
@@ -1,7 +1,8 @@
 
 const config = {
-    demoSiteUrl:"https://annotate-docs.dwaste.live/" 
+    DEMO_SITE_URL:"https://annotate-docs.dwaste.live/",
+    VITE_SERVER_URL: "http://localhost:5000",
   };
   
-  export default config;
+export default config;
   
