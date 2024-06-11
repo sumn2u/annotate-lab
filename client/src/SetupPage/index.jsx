03c15b1ec0ea16ff57e21c9f43419489e75950ec
harith-hacky03 <harithhacky3@gmail.com>
2024-06-11 15:36:20 +0530

    created config file for demosite url

--- client/src/SetupPage/index.jsx
+++ client/src/SetupPage/index.jsx
@@ -19,6 +19,7 @@ import { useSettings } from "../SettingsProvider";
 import { useTranslation } from "react-i18next"
 import { Info } from '@mui/icons-material';
 import NoteSection from "../NoteSection";
+import config from '../../config.js'; //importing demo site config from client/config.js
 
 const Container = styled("div")({
   marginTop: "2rem",
@@ -110,7 +111,7 @@ export const SetupPage = ({setConfiguration, settings, setShowLabel}) => {
                   <NoteSection 
                   icon={Info} 
                   text={t("more_info")} 
-                  link="https://annotate-docs.dwaste.live" 
+                  link={config.demoSiteUrl} 
                 />
                   <Box display="flex" paddingTop="5rem" justifyContent="end">
                         <Button variant="contained" disabled={settings.taskDescription.trim().length <= 0} onClick={() => setTab("configure")} disableElevation>
