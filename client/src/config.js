d0e3c4f95e90c3a24f73f863d1e20a775d1ab70c
sumn2u <sumn2u@gmail.com>
2024-06-16 13:02:59 -0500

    fixed docker config and refactor config

--- client/src/config.js
+++ client/src/config.js
@@ -1,15 +1,15 @@
 
-let VITE_SERVER_URL;
+let SERVER_URL;
 
 if (typeof import.meta !== 'undefined' && import.meta.env) {
-  VITE_SERVER_URL = import.meta.env.VITE_SERVER_URL;
+  SERVER_URL = import.meta.env.VITE_SERVER_URL;
 } else {
-  VITE_SERVER_URL = process.env.VITE_SERVER_URL || "http://localhost:5000";
+  SERVER_URL = process.env.VITE_SERVER_URL || "http://localhost:5000";
 }
 
 const config = {
-    DEMO_SITE_URL:"https://annotate-docs.dwaste.live/",
-    VITE_SERVER_URL,
+    DOCS_URL:"https://annotate-docs.dwaste.live/",
+    SERVER_URL,
     UPLOAD_LIMIT: 5,
   };
   
