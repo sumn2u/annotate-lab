431d4b3a5736f203d7e4a37d7682658a4ebca49e
DQ4443 <dq4443@gmail.com>
2024-06-18 21:41:25 -0700

    [Feat] Update outline thickness config var Updated OUTLINE_THICKNESS_CONFIG variable to be a struct to allow for higher granularity customization

--- server/config.py
+++ server/config.py
@@ -1,2 +1,6 @@
 MASK_BACKGROUND_COLOR = (0, 0, 0)
-OUTLINE_THICKNESS = 3 # change outline thickness (currently only for downloaded files)
+OUTLINE_THICKNESS_CONFIG = {
+    "POLYGON": 3,
+    "CIRCLE": 3,
+    "BOUNDING_BOX": 3
+} # change outline thickness (currently only for downloaded files)
