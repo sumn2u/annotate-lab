3fca9a789139f3277db890854732677c8fcdae59
sumn2u <sumn2u@gmail.com>
2024-07-23 09:23:59 -0500

    use browser default theme

--- client/src/ThemeContext/index.jsx
+++ client/src/ThemeContext/index.jsx
@@ -3,7 +3,18 @@ import React, { createContext, useState, useContext } from 'react';
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('dark'); // Default to light theme
+  const getPreferredColorScheme = () => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      return 'dark';
+    } else {
+      return 'light';
+    }
+  }
+  
+  const preferredColorScheme = getPreferredColorScheme();
+  console.log(`Preferred color scheme: ${preferredColorScheme}`);
+
+  const [theme, setTheme] = useState(preferredColorScheme); // Default to browser theme
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
