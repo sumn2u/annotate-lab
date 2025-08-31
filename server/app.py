2d364853c423435c2e3d3b725a6a71fd7397542b
Glenn Fung <117829736+glenntfung@users.noreply.github.com>
2025-08-31 18:00:15 -0500

    FIX(server): safe image mode

--- server/app.py
+++ server/app.py
@@ -544,7 +544,7 @@ def download_image_with_annotations():
                     )
 
                 response = requests.get(image_url)
-                image = Image.open(BytesIO(response.content))
+                image = Image.open(BytesIO(response.content)).convert("RGBA")
                 draw = ImageDraw.Draw(image)
 
                 for region in image_info.get("regions", []):
@@ -852,7 +852,7 @@ def download_image_mask():
 
                     response = requests.get(image_url)
                     response.raise_for_status()
-                    image = Image.open(BytesIO(response.content))
+                    image = Image.open(BytesIO(response.content)).convert("RGBA")
                     width, height = image.size
                     mask = Image.new(
                         "RGB", (width, height), app.config["MASK_BACKGROUND_COLOR"]
